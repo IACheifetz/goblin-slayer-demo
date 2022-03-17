0394f99f08e39ad1664505fa51b088dc87787ffb
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-17 12:12:59 -0700

    moved render function into own file and set up display for goblins

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // import functions and grab DOM elements
+import { renderGobbieEl } from './render-utils.js';
 const formEl = document.querySelector('form');
 const gobbieListEl = document.querySelector('.goblin-list');
 const defeatedGobbiesEl = document.querySelector('.defeated-goblins');
@@ -9,19 +10,20 @@ let playerHP = 10;
 let gobbiefolkDefeated = 0;
 const gobbies = [
     { name: 'Uptalks', hp: 3 }, 
-    { name: 'Longflops', hp: 1 }
+    { name: 'Longflops', hp: 2 }
 ];
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
 formEl.addEventListener('submit', (e) => {
     e.preventDefault();
     // alert('submitted form test');
+
+    const data = new FormData(formEl);
+
     const newGobbie = {
-        name: 'Downstalks',
-        hp: 4,
+        name: data.get('gobbie-name'),
+        hp: Math.ceil(Math.random() * 6),
     };
+
     gobbies.unshift(newGobbie);
 
     displayGobbies();
@@ -35,18 +37,10 @@ function displayGobbies() {
     gobbieListEl.textContent = '';
 
     for (let gobbie of gobbies) {
-        const gobbieEl = document.createElement('div');
-        const nameEl = document.createElement('p');
-        const HpEl = document.createElement('p');
-
-        gobbieEl.classList.add('gobbie');
-
-        nameEl.textContent = gobbie.name;
-        HpEl.textContent = gobbie.hp;
-
-        gobbieEl.append(nameEl, HpEl);
+        const gobbieEl = renderGobbieEl(gobbie);
 
         gobbieListEl.append(gobbieEl);
-}
+    }
 
 }
+
---
+++ render-utils.js
@@ -0,0 +1,31 @@
+export function renderGobbieEl(gobbie) {
+    const gobbieEl = document.createElement('div');
+    const nameEl = document.createElement('p');
+    const HpEl = document.createElement('p');
+    
+    gobbieEl.classList.add('gobbie');
+
+    let gobbiePic = '';
+
+    if (gobbie.hp === 3 || 
+        gobbie.hp === 1 || 
+        gobbie.hp === 2 || 
+        gobbie.hp === 4 || 
+        gobbie.hp === 5 || 
+        gobbie.hp === 6) {
+
+        gobbiePic = '👺';
+
+    } else if (gobbie.hp < 1) { 
+
+        gobbiePic = '💀';
+
+    }
+
+    nameEl.textContent = gobbie.name;
+    HpEl.textContent = `${gobbiePic} ${gobbie.hp}`;
+
+    gobbieEl.append(nameEl, HpEl);
+
+    return gobbieEl;
+}
